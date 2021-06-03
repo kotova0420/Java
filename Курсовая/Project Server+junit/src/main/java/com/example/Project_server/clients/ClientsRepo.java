d68ff27c218c6b2e0d3cddc880fc76a36b76582b
kotova0420 <62509420+kotova0420@users.noreply.github.com>
2021-06-03 18:22:05 +0300

    1

--- Курсовая/Project Server+junit/src/main/java/com/example/Project_server/clients/ClientsRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.Project_server.clients;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientsRepo extends JpaRepository<Clients, Long> {
-}
