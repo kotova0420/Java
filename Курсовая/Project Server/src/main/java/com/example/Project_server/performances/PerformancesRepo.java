2b05c323d4dbf580c4957f41a1c34eb2a481f8fa
kotova0420 <62509420+kotova0420@users.noreply.github.com>
2021-06-03 18:22:53 +0300

    1

--- Курсовая/Project Server/src/main/java/com/example/Project_server/performances/PerformancesRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.Project_server.performances;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerformancesRepo extends JpaRepository<Performances, Long> {
-}
--- Курсовая/Project Server/src/main/java/com/example/Project_server/shares/SharesRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.Project_server.shares;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SharesRepo extends JpaRepository<Shares, Long> {
-}
